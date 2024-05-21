df5dbec3360b6cde70821fdf79e7d1d3ca404fbd
alessandrodaniello105 <kanymsevement@gmail.com>
2024-05-21 20:19:56 +0200

    [edit]: add-musician-button is now a div

--- resources/views/livewire/add-musician-button.blade.php
+++ resources/views/livewire/add-musician-button.blade.php
@@ -1,1 +1,1 @@
-<button wire:click="$dispatch('update-list')" class="btn btn-light add-btn {{($isPicked)? 'disabled' : ''}}" id="new-add-{{$instrumentName}}">&rightarrow;</button>
+<div wire:click="$dispatch('update-list')" class="btn btn-light add-btn {{($isPicked)? 'disabled' : ''}}" id="new-add-{{$instrumentName}}">&rightarrow;</div>
