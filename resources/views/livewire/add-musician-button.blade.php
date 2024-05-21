23e67e3c84eac0b67fb4f397d9e756ca27295617
alessandrodaniello105 <kanymsevement@gmail.com>
2024-05-21 13:21:17 +0200

    [add]: dispatch update-list on click add-musician-button

--- resources/views/livewire/add-musician-button.blade.php
+++ resources/views/livewire/add-musician-button.blade.php
@@ -1,1 +1,1 @@
-<button class="btn btn-light add-btn {{($isPicked)? 'disabled' : ''}}" id="new-add-{{$instrumentName}}">&rightarrow;</button>
+<button wire:click="$dispatch('update-list')" class="btn btn-light add-btn {{($isPicked)? 'disabled' : ''}}" id="new-add-{{$instrumentName}}">&rightarrow;</button>
