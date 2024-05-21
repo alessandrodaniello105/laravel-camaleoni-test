7d78d751c79d15b0a4f04084717340e8cfd5c95b
alessandrodaniello105 <kanymsevement@gmail.com>
2024-05-21 20:22:59 +0200

    [add]: randomizer btn+modal in sidebar;[edit]: some manual band modal attrs

--- resources/views/bands/create.blade.php
+++ resources/views/bands/create.blade.php
@@ -29,19 +29,21 @@
         </div> --}}
         <div class="row justify-content-center ">
 
-            {{-- OPEN MANUAL BAND MODAL BUTTON --}}
+            {{-- MANUAL BAND MODAL OPEN BUTTON --}}
             <div class="open-modal-button-container d-md-none ">
-                <div class="open-modal" id="open-manual-band-modal" data-bs-toggle="modal" data-bs-target="#exampleModal"></div>
+                <div class="open-modal" id="open-manual-band-modal" data-bs-toggle="modal" data-bs-target="#openManualBandModal"></div>
             </div>
-            {{-- // OPEN MANUAL BAND MODAL BUTTON --}}
+            {{-- // MANUAL BAND MODAL OPEN BUTTON --}}
 
             <!-- Button trigger modal -->
             {{-- <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
                 Launch demo modal
             </button> --}}
+
+
             {{-- TODO: continue manual band into modal --}}
-            <!-- Modal -->
-            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <!-- MODAL -->
+            <div class="modal fade" id="openManualBandModal" tabindex="-1" aria-labelledby="manualBandModalLabel" aria-hidden="true">
 
                 <div class="modal-dialog">
 
@@ -49,7 +51,7 @@
 
                         <div class="modal-header">
 
-                            <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                            <h1 class="modal-title fs-5" id="manualBandModalLabel">Modal title</h1>
 
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 
@@ -71,6 +73,7 @@
 
                 </div>
             </div>
+            <!-- //MODAL -->
 
 
 
@@ -103,32 +106,113 @@
 @endsection
 
 @section('sidebar')
-<aside class=".col-4 d-none d-md-inline-block text-center align-self-center manual-band-list-container">
+
+
+
+<aside class=".col-4  text-center align-self-center manual-band-list-container">
     {{-- <p>
         seconda colonna a destra
     </p> --}}
+    <div class="mx-3">
+        {{-- RANDOMIZER MODAL OPEN BUTTON --}}
+        <button class="btn btn-outline-primary open-modal" id="randomizer-open-button" data-bs-toggle="modal" data-bs-target="#openRandomizerModal">
+            OPEN RANDOMIZER
+        </button>
+        {{-- // RANDOMIZER MODAL OPEN BUTTON --}}
 
-    <form id="test-form" action="{{route('bands.store')}}" method="POST">
-        @csrf
-        <div class="col-12 col-md-3 d-inline-block m-3 musicians-list-card">
+        {{-- TODO: continue randomizerr into modal --}}
+        <!-- MODAL -->
+        <div class="modal fade" id="openRandomizerModal" tabindex="-1" aria-labelledby="randomizerModalLabel" aria-hidden="true">
 
-            <div class="card py-2">
+            <div class="modal-dialog">
 
-                {{-- MANUAL SELECT MUSICIANS (CREATE BAND) COMPONENT --}}
-                <livewire:manual-band-session-list live  :musicians="$allMusicians" />
+                <div class="modal-content">
+
+                    <div class="modal-header">
+
+                        <h1 class="modal-title fs-5" id="randomizerModalLabel">Randomizer</h1>
+
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+
+                    </div>
+
+                    <div class="modal-body">
+                        <form id="randomizer-form" action="{{route('bands.store')}}" method="POST">
+                            @csrf
+
+                            {{-- RANDOMIZER FORM FLAG --}}
+                            <input type="checkbox" name="isRandomized" id="is-randomized" hidden checked>
+
+                            {{-- NEW BAND NAME --}}
+                            <div class="input-group mb-3">
+                                <span class="input-group-text" id="band-name-label">What's the band name?</span>
+                                <input type="text" class="form-control" name="bandName" id="band-name" placeholder="What's the band name?" aria-label="What's the band name?" aria-describedby="band-name-label">
+                            </div>
+                            {{-- //NEW BAND NAME --}}
+                            <hr>
+                            {{-- HOW MANY MUSICIANS IN NEW BAND --}}
+                            <div class="input-group mb-3">
+                                <span class="input-group-text" id="how-many-musicians-label">How Many Musicians?</span>
+                                <input
+                                  type="number"
+                                  min="2"
+                                  max="6"
+                                  class="form-control"
+                                  name="howManyMusicians"
+                                  id="how-many-musicians"
+                                  placeholder="How Many Musicians?"
+                                  aria-label="How Many Musicians?"
+                                  aria-describedby="how-many-musicians-label">
+                            </div>
+                            {{-- //HOW MANY MUSICIANS IN NEW BAND --}}
+
+
+
+                            <livewire:send-manual-band-form formName="randomizer-form" wire:model="$isDisabled" live />
+
+                        </form>
+                    </div>
+
+                    <div class="modal-footer">
+
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+
+                        <button type="button" class="btn btn-primary">Save changes</button>
+
+                    </div>
+
+                </div>
 
             </div>
-
-            @php
-                $counter = 6;
-                $counter = $counter - count($pickedInstruments);
-            @endphp
         </div>
-    </form>
+        <!-- //MODAL -->
+
+    </div>
+
+    <div class="d-none d-md-inline-block">
+        <form id="test-form" action="{{route('bands.store')}}" method="POST">
+            @csrf
+            <div class="col-12 col-md-3 d-inline-block m-3 musicians-list-card">
+
+                <div class="card py-2">
+
+                    {{-- MANUAL SELECT MUSICIANS (CREATE BAND) COMPONENT --}}
+                    <livewire:manual-band-session-list live  :musicians="$allMusicians" />
+
+                </div>
+
+                @php
+                    $counter = 6;
+                    $counter = $counter - count($pickedInstruments);
+                @endphp
+            </div>
+        </form>
 
 
 
-    <livewire:send-manual-band-form wire:model="$isDisabled" live />
+        <livewire:send-manual-band-form formName="test-form" wire:model="$isDisabled" live />
+
+    </div>
 
 </aside>
 @endsection
