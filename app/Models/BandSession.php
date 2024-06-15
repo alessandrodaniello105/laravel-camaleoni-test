96158e9063a9603828285527cf9576eafb103dbc
alessandrodaniello105 <kanymsevement@gmail.com>
2024-06-15 17:15:42 +0200

    [exp]: BandSession model {just begun}

--- app/Models/BandSession.php
+++ app/Models/BandSession.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class BandSession extends Model
 {
     use HasFactory;
+
+    public function __construct(
+                private Musician $drummer,
+                private Musician $bassist,
+                private Musician $guitarist,
+                private Musician $singer,
+                private Musician $otherist,
+                public bool $hasPlayed = false,
+    )
+    {
+    }
+
+
 }
